a7298e025525628f8e2dc7d940e04d36bd896fe8
NenadMilinkovic <nenadmilinkovic95@gmail.com>
2023-05-09 10:17:07 +0200

    Init app commit

---
+++ src/main/kotlin/com/vacation/tracker/dataImport/DataImportApplication.kt
@@ -0,0 +1,11 @@
+package com.vacation.tracker.dataImport
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataImportApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataImportApplication>(*args)
+}
---
+++ src/test/kotlin/com/vacation/tracker/dataImport/DataImportApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vacation.tracker.dataImport
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataImportApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
